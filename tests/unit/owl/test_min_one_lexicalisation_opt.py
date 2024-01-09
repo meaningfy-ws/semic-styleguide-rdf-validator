d8c69e4027c51b8d39b1964bf104a0da4c82e0f6
Rashif Ray Rahman <schiv@archlinux.org>
2024-01-10 01:28:15 +0600

    Add partial lexicalisation tests
    While SEMIC rule SC-R2 discusses recommended lexicalisation properties
    that constitute a _lightweight_ profile, it is not possible to "close"
    the shapes to restrict usage of properties to only those listed, as it
    would be too restrictive for practical use.
    
    There is an overlap of SC-R2 with SC-R3, which elaborates on recommended
    lexicalisation properties from a set of two vocabularies (`rdfs` and
    `skos`). Here, again, the restrictions would be impractical, and the
    shapes too complex.
    
    However, while not directly prescribed or specified by the rules, we can
    derive two other rules from them:
    
    1. There MUST only ever be at most one label value per language and
    without, for the two primary lexicalisation properties `rdfs:label` and
    `skos:prefLabel` (we may also later expand to `sh:name` for ShaCL)
    
    2. There SHOULD always be at least one of the two primary lexicalisation
    properties expressed for a given OWL resource (again, this will be
    different for ShACL)
    
    The former involves complex shapes requiring a "qualified value shape".
    Together, they ensure that best practices are maintained with regards to
    usage of lexicalisation properties (exactly one occurence of each in a
    language or plain).
    
    SEMIC rules: SC-R1 (partial), SC-R2 (partial)
    
    Co-authored-by: Ahmad, Jana, Ing <ahmadjan@fel.cvut.cz>

---
+++ tests/unit/owl/test_min_one_lexicalisation_opt.py
@@ -0,0 +1,51 @@
+import pytest
+from pyshacl import validate
+from rdflib import Graph
+
+from tests import SHAPES_FOLDER
+from tests.unit.owl import TEST_DATA_FOLDER
+
+TEST_FILE_NAME = "min_one_lexicalisation_opt"
+TEST_DATA_FOLDER = TEST_DATA_FOLDER / TEST_FILE_NAME
+SHAPES_FOLDER = SHAPES_FOLDER / "owl"
+
+
+@pytest.fixture
+def shacl_data():
+    # Load your SHACL shapes
+    return Graph().parse(SHAPES_FOLDER / f"{TEST_FILE_NAME}.shacl.ttl", format="ttl")
+
+
+@pytest.fixture
+def correct_data():
+    # Load correct RDF data
+    return Graph().parse(
+        TEST_DATA_FOLDER / f"{TEST_FILE_NAME}_correct.ttl", format="ttl"
+    )
+
+
+@pytest.fixture
+def wrong_data():
+    # Load incorrect RDF data
+    return Graph().parse(TEST_DATA_FOLDER / f"{TEST_FILE_NAME}_wrong.ttl", format="ttl")
+
+
+def test_shacl_validation_correct(shacl_data, correct_data):
+    # Validate correct RDF data against SHACL shapes
+    # NOTE: No inference here otherwise vocab superclasses raise violations
+    conforms, report_graph, _ = validate(correct_data, shacl_graph=shacl_data)
+
+    # Assert that the data conforms to the shapes
+    assert conforms, f"SHACL validation failed:\n{report_graph.serialize()}"
+
+
+def test_shacl_validation_wrong(shacl_data, wrong_data):
+    # Validate incorrect RDF data against SHACL shapes
+    conforms, _, _ = validate(wrong_data, shacl_graph=shacl_data, inference="rdfs")
+
+    # Assert that the data does not conform to the shapes
+    assert not conforms, "SHACL validation succeeded, but it should have failed"
+
+
+if __name__ == "__main__":
+    pytest.main()
