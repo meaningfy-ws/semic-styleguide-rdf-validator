c56f3781ae5b92b5fcc5da3388db1f587edf3b25
Rashif Ray Rahman <schiv@archlinux.org>
2024-01-12 03:01:30 +0600

    Add a SHACL runner and some examples
    We expect to allow users to run validations using the (aggregated or
    modular) shapes against their own data and without having to run tests,
    so a convenient PySHACL validation helper script has to be present.
    Example data to go along with is always useful to have as well.

---
+++ validation_runner.py
@@ -0,0 +1,26 @@
+from pyshacl import validate
+from rdflib import Graph
+
+DATA_FILE = "resources/examples/example-data.ttl"
+SHACL_FILE = "resources/examples/example-shape.shacl.ttl"
+INFERENCE = "rdfs"  # comment out if undesired
+
+
+def main(data_file, shacl_file):
+    input_data = Graph().parse(data_file)
+    shacl_data = Graph().parse(shacl_file)
+    # conforms, report_graph, text = validate(input_data, shacl_graph=shacl_data)
+    _, _, text = validate(input_data, shacl_graph=shacl_data, inference=INFERENCE)
+
+    print(text)
+
+    # if not conforms:
+    #     import json
+    #     result_list = json.loads(report_graph.serialize(format="json-ld"))
+    #     result_message = [d.get("http://www.w3.org/ns/shacl#resultMessage", None) for d in result_list]
+    #     print(result_message[1][0]["@value"])
+    #     print(report_graph.serialize())
+
+
+if __name__ == "__main__":
+    main(DATA_FILE, SHACL_FILE)
